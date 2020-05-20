5ea78c2bbebe5d17e0dc2293c2d5798f7020dc5e
MilBia <milosz.bialczak@gmail.com>
2020-05-20 19:31:47 +0200

    update/fic some frontend

--- src/main/resources/public/scripts/topBar.js
+++ src/main/resources/public/scripts/topBar.js
@@ -1,8 +1,12 @@
 var topBarApp = angular.module('topBarApp', []);
 
-topBarApp.controller('topBarCtrl', function ($scope, $location) {
+topBarApp.controller('topBarCtrl', function ($scope, $http) {
         $scope.changeView = function (path) {
             window.location = path;
+        }
+
+        $scope.UpdateData = function () {
+            $http.get('http://localhost:8080/infections/new_data').then(function (response) {})
         }
     }
 );
